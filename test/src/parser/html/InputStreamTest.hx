54e13c298883e336992828d3a890366cb9c94d6f
Thomas Fetiveau <thomas.fetiveau.tech@gmail.com>
2013-04-24 22:10:17 +0200

    fix unit tests

--- test/src/parser/html/InputStreamTest.hx
+++ test/src/parser/html/InputStreamTest.hx
@@ -25,57 +25,57 @@ class InputStreamTest
 	{
 		var is : InputStream = new InputStream( "Colone" );
 		var r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([-2], r);
 		
 		is = new InputStream( "Colone;" );
 		r = is.nextCharRef();
-		Assert.equals([0x2A74], r);
+		Assert.same([0x2A74], r);
 		
 		is = new InputStream( "rien" );
 		r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([-2], r);
 		
 		is = new InputStream( "dfg56dfgd4;" );
 		r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([-2], r);
 		
 		is = new InputStream( "j djz jzjkld zdke" );
 		r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([-2], r);
 		
 		is = new InputStream( "Atilde" );
 		r = is.nextCharRef();
-		Assert.equals([0xC3], r);
+		Assert.same([0xC3], r);
 		
 		is = new InputStream( "" );
 		r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([-2], r);
 		
 		is = new InputStream( "#xCCCCCC;" );
 		r = is.nextCharRef();
-		Assert.equals([-2], r);
+		Assert.same([0xFFFD], r);
 	}
 	
 	public function testConsumeUntilString()
 	{
 		var is : InputStream = new InputStream( "blablabla ]]>" );
 		var r = is.consumeUntilString("]]>");
-		Assert.equals([0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x20], r);
+		Assert.same([0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x20], r);
 		Assert.equals( '>'.code, is.currentInputChar() );
 		
 		var is : InputStream = new InputStream( "blablabla ]]> blablabla" );
 		var r = is.consumeUntilString("]]>");
-		Assert.equals([0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x20], r);
+		Assert.same([0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x62, 0x6C, 0x61, 0x20], r);
 		Assert.equals( '>'.code, is.currentInputChar() );
 		
 		var is : InputStream = new InputStream( "tra" );
 		var r = is.consumeUntilString("]]>");
-		Assert.equals([0x74, 0x72, 0x61], r);
+		Assert.same([0x74, 0x72, 0x61], r);
 		Assert.equals( -1, is.currentInputChar() );
 		
 		var is : InputStream = new InputStream( "" );
 		var r = is.consumeUntilString("]]>");
-		Assert.equals([], r);
+		Assert.same([], r);
 		Assert.equals( -1, is.currentInputChar() );
 	}
 	
